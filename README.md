eb95e3c9cc18c52d7a8d3ea4e8b747976542dc5f
xGuache <jalejovz@gmail.com>
2022-08-27 16:02:21 -0500

    actualizacion

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Reto5
-Reto5Java MisionTic
+# Reto5Java MisionTic 
+Este programa, crea una interfaz de usuario para manipular elementos en una base de datos MySQL.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
+import MyJDBC.MyJDBC;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        // MyJDBC NewJDBC = new MyJDBC();
-        //System.out.println(NewJDBC.DB_Connection());
+        MyJDBC NewJDBC = new MyJDBC();
+        System.out.println(NewJDBC.DB_Connection());
     }
 }
